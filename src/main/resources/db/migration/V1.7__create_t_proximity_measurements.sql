7824fbc3dd296249d67643b06ddd6ea5e5c83ae8
margawron <max2.marcel@gmail.com>
2020-12-22 00:26:37 +0100

    Fix ProximityMeasurements ddl script.

--- src/main/resources/db/migration/V1.7__create_t_proximity_measurements.sql
+++ src/main/resources/db/migration/V1.7__create_t_proximity_measurements.sql
@@ -5,11 +5,10 @@ create table t_proximity_measurements(
     prx_type   text
 );
 
-alter table t_proximity_measurements
-    add constraint pk_t_proximity_measurements_prx_id
-        primary key (prx_id);
+alter table t_proximity_measurements add constraint pk_t_proximity_measurements_prx_id
+    primary key (prx_id);
 
-alter table t_proximity_measurements add constraint fk_t_proximity_measurements_prx_alr_id
+alter table t_proximity_measurements add constraint fk_t_proximity_measurements_prx_msr_id
     foreign key (prx_msr_id) references t_location_measurements (msr_id);
 
 alter table t_proximity_measurements add constraint fk_t_proximity_measurements_prx_alr_id
