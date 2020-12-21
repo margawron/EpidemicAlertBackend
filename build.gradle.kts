809ddfa18143f4fd4c8d08c76990740b3fcaf95e
margawron <max2.marcel@gmail.com>
2020-12-21 23:24:07 +0100

    Add firebase admin sdk as dependency. Setup firebase config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 
-    
+    implementation("com.google.firebase:firebase-admin:6.8.1")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
