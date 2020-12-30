89729fb269abf701598e555b72ea06edcd3a2992
margawron <max2.marcel@gmail.com>
2020-12-30 04:10:37 +0100

    Add transactional for device registration method.

--- src/main/kotlin/com/github/margawron/epidemicalert/device/DeviceService.kt
+++ src/main/kotlin/com/github/margawron/epidemicalert/device/DeviceService.kt
@@ -3,12 +3,14 @@ package com.github.margawron.epidemicalert.device
 import com.github.margawron.epidemicalert.users.LoginDto
 import com.github.margawron.epidemicalert.users.User
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
 class DeviceService(
-    private val deviceRepository: DeviceRepository
+    private val deviceRepository: DeviceRepository,
 ) {
 
+    @Transactional
     fun registerDeviceOrUpdateFirebaseToken(user: User, loginRequest:LoginDto): Device {
         val device = user.userDevices.find { device -> device.serialNumber == loginRequest.serialNumber && device.manufacturer == loginRequest.manufacturer}
         return if(device == null){
