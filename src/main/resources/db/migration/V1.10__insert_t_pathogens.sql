149ec9acee1157bfd089cfcbc7016b1efc7cd145
margawron <max2.marcel@gmail.com>
2021-01-10 21:11:58 +0100

    Add script for filling intital pathogen table.

---
+++ src/main/resources/db/migration/V1.10__insert_t_pathogens.sql
@@ -0,0 +1,7 @@
+insert into t_pathogens
+(pat_name, pat_contagious_period, pat_period_resolution, pat_detection_range, pat_accuracy)
+values ('A/H1N1v', 12, 'D', 11, 2),
+        ('SARS-CoV-2', 12, 'D', 11, 2),
+        ('EBOV', 12, 'D', 11, 2),
+        ('MERS-CoV', 12, 'D', 11, 2),
+        ('HPAI', 12, 'D', 11, 2);
